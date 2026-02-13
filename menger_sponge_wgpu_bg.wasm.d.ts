a0e8c68732aefd891b26aeafbe84e46bf3508cef
rayonreywa-bit <rayonreywa@gmail.com>
2026-02-13 04:56:52 +0300

    Add files via upload

---
+++ menger_sponge_wgpu_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const run: () => void;
+export const wasm_bindgen__closure__destroy__hbe34a2d7cb59ceb5: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h0cb18a44df41320f: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__heabac7dcee54b463: (a: number, b: number) => void;
+export const wasm_bindgen__closure__destroy__h33cb5d910787be9e: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__ha33b0efba46a77a1: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h58ccf1f2efb0a12f: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h5aa321ae20b28bd6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h8712f3a1e8995eae: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h8499ef5b94e0dad0: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__hd0839a62e0935548: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
